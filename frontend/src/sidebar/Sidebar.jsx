b4f225c6657b8a8859a7edf7dadd144c9cb2ce9c
HeRo3S <hungid6a1@gmail.com>
2022-07-26 15:13:08 +0700

    updated sidebar component
    updated sidebar component
    created post component

--- frontend/src/sidebar/Sidebar.jsx
+++ frontend/src/sidebar/Sidebar.jsx
@@ -1,7 +1,24 @@
 import './sidebar.css'
 
-export default function sidebar() {
+export default function Sidebar() {
   return (
-    <div className='sidebar'>sidebar</div>
+    <div className='sidebar'>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img className='sidebarAvatar' src={require('../image/violet.jpg')} alt="sidebar avatar" />
+        <p className='sidebarDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec bibendum nibh. Nulla consequat magna sed nibh pretium pretium. Nam feugiat porttitor sollicitudin.</p>
+      </div>
+      <div className="sidebarItem">
+        <span className='sidebarTitle'>CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Anime</li>
+          <li className="sidebarListItem">Visual Novel</li>
+          <li className="sidebarListItem">Game</li>
+          <li className="sidebarListItem">Manga</li>
+          <li className="sidebarListItem">Self promotoion</li>
+          <li className="sidebarListItem">Others</li>
+        </ul>
+      </div>
+    </div>
   )
 }

a1dab7465fa13cce1de1e2044ff6b27944d13c0d
HeRo3S <hungid6a1@gmail.com>
2022-08-02 23:32:04 +0700

    added mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 
-console.log("Yo");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useCreateIndex: true,
+  })
+  .then(console.log("Connect to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/", (req, res) => {
+  console.log("hey this is main url");
+});
 
 app.listen("5000", () => {
   console.log("Backend is running.");

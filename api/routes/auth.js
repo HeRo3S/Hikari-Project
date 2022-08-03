6c03e0bfdc0699b20aba21601da36939537b1516
HeRo3S <hungid6a1@gmail.com>
2022-08-03 14:28:26 +0700

    finished authentication function

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -14,13 +14,28 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    //login function here
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(400).json("Wrong credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) return res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //ROUTER
 module.exports = router;

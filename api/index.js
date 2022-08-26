5d065633704635d61a384ae0c699ad50bd48fbc3
HeRo3S <hungid6a1@gmail.com>
2022-08-26 11:03:36 +0700

    added crud post functions

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -20,6 +21,7 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running.");
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,16 +5,21 @@ const bcrypt = require("bcrypt");
 
 //UPDATE
 router.put("/:id", async (req, res) => {
-  console.log(req.body);
   if (req.body.userId === req.params.id) {
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
       res.status(200).json(updatedUser);
     } catch (err) {}
   } else {
